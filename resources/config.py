444adca54e90b3ec787f8673460540898c1ec2fd
Joel Ocay <jmocay72@gmail.com>
2020-05-18 09:03:15 -0400

    origin

--- resources/config.py
+++ resources/config.py
@@ -1,5 +1,5 @@
 class AppConfig(object):
     ENV = 'PROD'
     NEWSFILE_REFRESH_TIME = 2 # hours
-    NEWSAPI_KEY = '9442c73891f7484b8397f85afa73ef25' # Site specific newsapi key
+    NEWSAPI_KEY = '' # Site specific newsapi key
 
