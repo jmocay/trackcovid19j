8a4834ab6584eb326facdb4c7e6611f968f433da
Joel Ocay <jmocay72@gmail.com>
2021-03-29 12:34:42 -0400

    heroku

--- resources/config.py
+++ resources/config.py
@@ -1,4 +1,4 @@
 class AppConfig(object):
     ENV = 'PROD'
     NEWSFILE_REFRESH_TIME = 2 # hours
-    NEWSAPI_KEY = '' # Site specific newsapi key
+    NEWSAPI_KEY = '9442c73891f7484b8397f85afa73ef25s' # Site specific newsapi key
